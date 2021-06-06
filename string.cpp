268bd9d9f87117a0d1eb86e61dc5f777e93eb7d3
user name <kinal27213@gmail.com>
2021-06-06 14:52:28 +0530

    your message for this commit

---
+++ string.cpp
@@ -0,0 +1,79 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char string[20],i;
+//	printf("Enter string:-");
+//	gets(string);
+//	for(i=0;string[i]!='\0';i++)
+//	{
+//		if(string[i]>='A' && string[i]<='Z')
+//		{
+//			string[i]=string[i]+32;
+//		}
+//		else if(string[i]>='a' && string[i]<='z')
+//		{
+//			string[i]=string[i]-32;
+//		}
+//	}
+//	puts(string);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char string[20],i,var,f=0,count;
+//	printf("enter string\n");
+//	gets(string);
+//	printf("enter word u want to search");
+//	scanf("%c",&var);
+//	for(i=0;string[i]!='\0';i++)
+//	{
+//		if(string[i]==var)
+//		{
+//			f=1;
+//			count=i;
+//			break;
+//		}	
+//	}
+//	if(f==1)
+//	{
+//		printf("%d",i);
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
